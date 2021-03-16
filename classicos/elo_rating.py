02763d865335b4254a169c34f4f3deed4f4c586f
Renan Tomazini <renantomazini@gmail.com>
2021-03-16 17:33:53 -0300

    Corrigindo update errado
    Agora sim é um Elo Rating.

--- classicos/elo_rating.py
+++ classicos/elo_rating.py
@@ -1,49 +1,30 @@
-class Ponto():
-    def __init__(self,nome,x,y):
-        self.nome = nome
-        self.x = x
-        self.y = y
-        distancias = []
+class Jogador:
+    def __init__(self,nome,elo):
+        self._nome = nome
+        self._elo = elo
 
-    def distancia(self, Ponto):
-        x = self.x - Ponto.x
-        y = self.y - Ponto.y
-        d = (x**2+y**2)**0.5
-        return(d)
 
-class Caminho():
-    def __init__(self, pontoA, pontoB):
-        self.pontoA = pontoA
-        self.pontoB = pontoB
-        self.distancia = pontoA.distancia(pontoB)
+Jorge = Jogador("Jorge",100)
+Ricardo = Jogador("Ricardo",130)
+Ana = Jogador("Ana",500)
+Lucia = Jogador("Lucia", 344)
+Carlos = Jogador("Carlos",223)
+Paula = Jogador("Paula", 234)
 
-a = Ponto('a',1,1)
-b = Ponto('b',3,2)
-c = Ponto('c',4,5)
-d = Ponto('d',10,5)
-e = Ponto('e',7,5)
-f = Ponto('f',2,1)
-g = Ponto('g',3,3)
+def eloRating(Ra,Rb):
+    chances = 1/(1+10**((Rb._elo-Ra._elo)/400))
+    print("A chance de " + Ra._nome + " derrotar " + Rb._nome +  " é : {:.2f}%".format(chances))
+    return chances
 
-pontos = [a,b,c,d,e,f,g]
-caminhos = []
-for ponto in pontos:
-    for p in pontos:
-        cam =Caminho(ponto, p)
-        if cam.distancia != 0.0:
-            caminhos.append(cam)
-
-caminhos_ordenados = sorted(caminhos,key = lambda caminho: caminho.distancia)
-
-pontos_conectados = []
-kruskal = []
-for cam in caminhos_ordenados:
-    if cam.pontoA not in pontos_conectados:
-        if cam.pontoB not in pontos_conectados:
-            kruskal.append(cam)
-            pontos_conectados.append(cam.pontoA)
-
-for pontos in pontos_conectados:
-    print(pontos.nome)
-for element in kruskal:
-    print("O ponto {} está conectado ao {} pela distancia {}".format(element.pontoA.nome,element.pontoB.nome,element.distancia))
+Jogadores = [Jorge,Ricardo,Ana,Lucia,Carlos,Paula]
+for jogador in Jogadores:
+    print("")
+    print(jogador._nome)
+    print("")
+    for oponente in Jogadores:
+        if jogador == oponente:
+            pass
+        else:
+            p = eloRating(jogador,oponente)
+            if p < 0.5:
+                jogador._elo = jogador._elo + 30
