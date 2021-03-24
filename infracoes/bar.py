01f02ac7223384eeee0fdbaa7e1d4f9212e5b4f1
Renan Tomazini <renantomazini@gmail.com>
2021-03-24 16:32:41 -0300

    Add files via upload

---
+++ infracoes/bar.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import pandas as pd
+
+dados = pd.read_csv('C:/Users/Tomazini/Desktop/bar_race/infracoes2020completo.csv', sep=';')
+dados.head()
+infracoes = dados.descricaoinfracao.unique()
+
+infracoes = dados["descricaoinfracao"].value_counts().to_frame().reset_index()
+
+infracoes.head(10)
+
+import matplotlib.pyplot as plt
+g = sns.barplot(y ="index",x="descricaoinfracao", data = infracoes.head(10))
+g.figure.savefig('a.jpg', figsize='large')
